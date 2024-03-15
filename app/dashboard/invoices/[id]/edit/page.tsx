8875378b78eea796bbc1e1b65bb79185103a1b08
Gabe Mata <gabemata@gmail.com>
2024-03-15 18:09:43 +0100

    chapter 14

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,19 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation';
+
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-      ]);
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers()
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
@@ -16,8 +22,8 @@ export default async function Page({ params }: { params: { id: string } }) {
           {
             label: 'Edit Invoice',
             href: `/dashboard/invoices/${id}/edit`,
-            active: true,
-          },
+            active: true
+          }
         ]}
       />
       <Form invoice={invoice} customers={customers} />
